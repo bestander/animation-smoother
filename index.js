513c1bb72f8dedaeb57f0ab3f8ce83b9740075e1
Konstantin Raev <bestander@gmail.com>
2012-12-30 15:41:32 +1300

    first test spec

--- index.js
+++ index.js
@@ -3,13 +3,23 @@
  */
 
 "use strict";
+var TWEEN = require('tween.js');
 
-function ObjectCoordinateInterpolator () {
-  
+
+function ObjectCoordinateInterpolator (initialCoordinate) {
+  this._tween = new TWEEN.Tween(initialCoordinate);
 }
 
 module.exports = ObjectCoordinateInterpolator;
 
 ObjectCoordinateInterpolator.prototype.updateAll = function () {
-  
+  TWEEN.update();
+};
+
+ObjectCoordinateInterpolator.prototype.scheduleNext = function (coordinate, delay) {
+  this._tween.to(coordinate, delay).start();
+};
+
+ObjectCoordinateInterpolator.prototype.onCoordinateRequest = function (callback) {
+  this._tween.onUpdate(callback);
 };
--- tests/interpolator.spec.js
+++ tests/interpolator.spec.js
@@ -7,10 +7,31 @@
 /*global it:true describe:true expect:true spyOn:true beforeEach:true afterEach:true jasmine:true window runs waitsFor*/
 "use strict";
 
-require('animation-smoother/index.js');
+var Interpolator = require('animation-smoother/index.js');
+
 
 describe('Object Coordinate Interpolator', function () {
-  it('should be awesome', function () {
-    
+
+  it('should return coordinates evenly spread between current and destination within a specified time period', function () {
+    var interpolator = new Interpolator({x: 100, y: 200});
+    var currentPosition;
+    var currentTime = Date.now();
+
+    interpolator.onCoordinateRequest(function () {
+      currentPosition = {x: this.x, y: this.y};
+    });
+    interpolator.scheduleNext({x: 400, y: 400}, 1000);
+
+    spyOn(Date, 'now').andReturn(currentTime + 1);
+    interpolator.updateAll();
+    expect(currentPosition).toEqual({x: 100, y: 200});
+
+    Date.now.andReturn(currentTime + 500);
+    interpolator.updateAll();
+    expect(currentPosition).toEqual({x: 100, y: 200});
   });
 });
+
+
+
+
